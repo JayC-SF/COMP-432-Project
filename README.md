1d4ceb9ea7b3ffd9f4ffcc98c5b539fda6ae7fa6
JayC-SF <srengfloresj@hotmail.com>
2026-04-17 22:44:53 -0400

    save preprocessing code for CNNs

--- README.md
+++ README.md
@@ -5,8 +5,15 @@ This project uses MiniConda for the python environment as it is simpler to deal 
 Please find the MiniConda user guide to install it on your computer.
 Please also find the environment.yml file to see all the libraries installed for this project.
 
+If you do not have Miniconda installed, please find this link for setting it up.
 
-Create a conda environment using the following command:
+If you alread have Miniconda installed, make sure to update to the latest version as follows:
+```sh
+conda update -n base -c defaults conda
+```
+
+
+To create the conda environment for this project use the following command:
 ```sh
 conda env create -f environment.yml -n <new_env_name>
 ```
--- src/variables.py
+++ src/variables.py
@@ -11,13 +11,16 @@ except ImportError:
 # take sensitive data from dotenv or colab secrets
 if IN_COLAB:
     AUDIO_ZIP_GID = userdata.get('AUDIO_ZIP_GID')
-    METADATA_ZIP_GID = os.getenv('METADATA_ZIP_GID')
-    MEL_SPECTOGRAM_ZIP_GID = os.getenv('MEL_SPECTOGRAM_ZIP_GID')
+    METADATA_ZIP_GID = userdata.get('METADATA_ZIP_GID')
+    MEL_SPECTOGRAM_ZIP_GID = userdata.get('MEL_SPECTOGRAM_ZIP_GID')
+    MEL_SPECTOGRAM_NPZ_GID = userdata.get('MEL_SPECTOGRAM_NPZ_GID')
 
 else:
     load_dotenv()
     AUDIO_ZIP_GID = os.getenv('AUDIO_ZIP_GID')
     METADATA_ZIP_GID = os.getenv('METADATA_ZIP_GID')
+    MEL_SPECTOGRAM_ZIP_GID = os.getenv('MEL_SPECTOGRAM_ZIP_GID')
+    MEL_SPECTOGRAM_NPZ_GID = os.getenv('MEL_SPECTOGRAM_NPZ_GID')
 
 DATA_PATH = Path('data')
 
@@ -29,3 +32,6 @@ METADATA_ZIP_FILE_PATH = DATA_PATH/'metadata.zip'
 
 MEL_SPECTOGRAM_PATH = DATA_PATH/'mel_spectogram'
 MEL_SPECTOGRAM_ZIP_FILE_PATH = DATA_PATH/'mel_spectogram.zip'
+MEL_SPECTOGRAM_NPZ_FILE_PATH = DATA_PATH/'mel_spectogram_audio_length_adjusted.npz'
+
+RUNS_PATH = Path('runs')
